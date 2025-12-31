2085d673eb2947c77cb22348303ade8c83c97405
ChaeHaejoong2 <chj03290329@gmail.com>
2025-12-31 18:31:46 +0000

    feat(posts): post 스키마 구조 변경

--- server/src/posts/dtos/create-post.dto.ts
+++ server/src/posts/dtos/create-post.dto.ts
@@ -3,9 +3,10 @@ import { IsString, IsNotEmpty, MaxLength } from 'class-validator';
 export class CreatePostDto {
   @IsString()
   @IsNotEmpty()
-	@MaxLength(100)
+  @MaxLength(100)
   title: string;
 
   @IsString()
+  @IsNotEmpty()
   content: string;
 }
