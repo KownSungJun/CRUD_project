2981788f0f232b8568805cfc95bf1260382d5d30
권성준 <58690800+KownSungJun@users.noreply.github.com>
2026-01-04 01:13:27 +0000

    develop 브랜치 이동

--- client/src/api/comments.js
+++ client/src/api/comments.js
@@ -9,7 +9,7 @@ import api from './axios';
  * @param {number=} params.limit             // 기본값은 서버 DTO
  */
 export const getComments = (params) => {
-  return api.get('/posts', {
+  return api.post('https://turbo-fishstick-95wpjj594vv2xx4j-3000.app.github.dev/api/posts', {
     params,
   });
 };
