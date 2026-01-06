9d3e0a08da43f02eacc760df0bceb45a778676b1
ChaeHaejoong2 <chj03290329@gmail.com>
2026-01-06 19:04:19 +0000

    vite config 프록시 서버 경로 수정 & README.md 파일에 api문서 경로 추가

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
-# CRUD_project
+# crud-project
+
+## API Documentation (Swagger)
+
+### Swagger UI
+
+- Path: `/api-docs`
+
+### 실행 방법 (Codespaces)
+
+1. NestJS 서버 실행
+2. 해당 포트의 Public URL 접속
+3. URL 뒤에 `/api-docs` 추가
--- client/vite.config.js
+++ client/vite.config.js
@@ -1,16 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
   server: {
     proxy: {
-      '/api': {
-        target: 'https://turbo-fishstick-95wpjj594vv2xx4j-3000.app.github.dev',
+      "/api": {
+        target: "http://localhost:3000",
         changeOrigin: true,
         secure: false,
-      }
-    }
+      },
+    },
   },
   plugins: [react()],
-})
+});
