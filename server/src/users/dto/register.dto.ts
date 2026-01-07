e1eccf84144d1e156351fab588dffbff27412f82
권성준 <58690800+KownSungJun@users.noreply.github.com>
2026-01-07 13:09:55 +0000

    register git conflict 수정

--- server/src/users/dto/register.dto.ts
+++ server/src/users/dto/register.dto.ts
@@ -5,14 +5,7 @@ export class RegisterDto {
   @ApiProperty({ example: 'haejoong123' })
   @IsString()
   @IsNotEmpty()
-<<<<<<< HEAD
-  // @Min(1) => 여기서 정수 관련 길이 처리를 해서     "userId must not be greater than 20", "userId must not be less than 1" 뜸
-  // @Max(20)
-  @MinLength(1)
-  @MaxLength(20)
-=======
   @Length(1, 20)
->>>>>>> 9d3e0a08da43f02eacc760df0bceb45a778676b1
   userId: string;
 
   @ApiProperty({ example: 'password!' })
