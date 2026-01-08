d706f5534fc4bdc8cf144ce393d1e9ada67c2ae2
권성준 <58690800+KownSungJun@users.noreply.github.com>
2026-01-08 11:01:19 +0000

    axios baseurl 상대경로 변경

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'https://turbo-fishstick-95wpjj594vv2xx4j-3000.app.github.dev/api',
+    baseURL: ' /api',
     withCredentials: true,
 });
 
