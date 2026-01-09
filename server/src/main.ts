85014fe251a79561ad21803af761dc6af192169c
권성준 <58690800+KownSungJun@users.noreply.github.com>
2026-01-09 06:36:15 +0000

    server 수정 code 삭제

--- server/src/main.ts
+++ server/src/main.ts
@@ -5,12 +5,6 @@ import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors({
-    origin: ['https://turbo-fishstick-95wpjj594vv2xx4j-5173.app.github.dev'],
-    methods: ['GET', 'POST', 'PATCH', 'DELETE', 'OPTIONS'],
-    allowedHeaders: ['Content-Type', 'Authorization'],
-    credentials: true, // 쿠키/인증 헤더 허용
-  });
   app.setGlobalPrefix('api');
   app.useGlobalPipes(
     new ValidationPipe({
